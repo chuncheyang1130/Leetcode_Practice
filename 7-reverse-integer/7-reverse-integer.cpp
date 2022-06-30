ce3f063da85a866fb1146b4c05964d9614d90f35
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-06-30 17:16:07 +0800

    Time: 0 ms (100.00%), Space: 6 MB (7.76%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string num = to_string(x);
+        std::reverse(num.begin(), num.end());
+        
+        if(num.back() == '-')
+            num = "-" + num.substr(0, num.size()-1);
+        
+        long long value = stoll(num, nullptr, 10);
+        if(value < INT_MIN || value > INT_MAX)
+            return 0;
+        else return (int)value;
+    }
+};
