68947751bb98e27949f2c3d9743195081103e4d7
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-01 23:07:12 +0800

    Time: 2 ms (57.49%), Space: 7.6 MB (75.56%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev_rob_max = 0, cur_rob_max = nums[0];
+        
+        for(int i = 1; i < nums.size(); i++){
+            swap(prev_rob_max, cur_rob_max);
+            cur_rob_max = max(cur_rob_max+nums[i], prev_rob_max);    
+        }
+        return cur_rob_max;
+    }
+};
