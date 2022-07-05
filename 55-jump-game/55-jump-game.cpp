58f0d6e3dd93d5041e83f52cbb909568337df000
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-05 10:48:25 +0800

    Time: 116 ms (41.47%), Space: 48.2 MB (93.30%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        if(nums.size() == 1)
+            return true;
+        int l = 0, r = nums.size()-1;
+        int cur_jump = l;
+        for(int i = 0; i < nums.size(); i++){
+            cur_jump = max(cur_jump, i+nums[i]);
+            if(i == l){
+                if(l == cur_jump && l < r)
+                    return false;
+                else l = cur_jump;
+            }
+        }
+        if(l >= r)
+            return true;
+        else return false;
+    }
+};
