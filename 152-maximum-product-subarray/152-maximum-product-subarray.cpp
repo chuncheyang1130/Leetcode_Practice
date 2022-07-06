43b29ac567ccbd13660c8ae9e1ec29a1cfaaf6a4
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-06 12:08:20 +0800

    Time: 8 ms (75.80%), Space: 13.6 MB (94.63%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -2,27 +2,39 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         
-        long long product = 1, cur_max = INT_MIN, cur_min_pos = 1, cur_min_neg = 1;
-        bool zero = false;
-        for(int i = 0; i < nums.size(); i++){
-            product *= nums[i];
-            cur_max = max({cur_max, product/cur_min_pos, product/cur_min_neg});
-            if(product > 0)
-                cur_min_pos = min(cur_min_pos, product);
-            else if(product < 0 && cur_min_neg == 1){
-                cur_min_neg = product;
-            }else if(product < 0 && cur_min_neg != 1){
-                cur_min_neg = max(cur_min_neg, product);
-            }else{
-                zero = true;
-                cur_min_neg = 1;
-                cur_min_pos = 1;
-                product = 1;
-            }
-        }
+//         long long product = 1, cur_max = INT_MIN, cur_min_pos = 1, cur_min_neg = 1;
+//         bool zero = false;
+//         for(int i = 0; i < nums.size(); i++){
+//             product *= nums[i];
+//             cur_max = max({cur_max, product/cur_min_pos, product/cur_min_neg});
+//             if(product > 0)
+//                 cur_min_pos = min(cur_min_pos, product);
+//             else if(product < 0 && cur_min_neg == 1){
+//                 cur_min_neg = product;
+//             }else if(product < 0 && cur_min_neg != 1){
+//                 cur_min_neg = max(cur_min_neg, product);
+//             }else{
+//                 zero = true;
+//                 cur_min_neg = 1;
+//                 cur_min_pos = 1;
+//                 product = 1;
+//             }
+//         }
         
-        if(zero)
-            cur_max = max((int)cur_max, 0);
-        return (int)cur_max;
+//         if(zero)
+//             cur_max = max((int)cur_max, 0);
+//         return (int)cur_max;
+        
+        int local_max = nums[0], local_min = nums[0];
+        int M = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < 0)
+                swap(local_max, local_min);
+            local_max = max(nums[i], nums[i]*local_max);
+            local_min = min(nums[i], nums[i]*local_min);
+            
+            M = max(local_max, M);
+        }
+        return M;
     }
 };
