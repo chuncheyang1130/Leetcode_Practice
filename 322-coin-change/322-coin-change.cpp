7755f9f8768c99c22e4aaaed63ab9946b39cfe4f
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-16 23:15:56 +0800

    Time: 185 ms (26.12%), Space: 14.4 MB (69.27%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(), coins.end());
         vector<int> dp(amount+1, INT_MAX);
         dp[0] = 0;
         for(int i = 1; i <= amount; i++){
