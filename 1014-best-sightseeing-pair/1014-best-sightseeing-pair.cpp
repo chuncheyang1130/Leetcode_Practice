235eff40eda7bc69ec6e7cede6b20f7628d074ac
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-22 23:03:54 +0800

    Time: 124 ms (9.77%), Space: 39.3 MB (84.55%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        vector<int> front(values.size());
-        front[0] = values[0];
-        int M = front[0] + values[1] - 1;
+        int front = values[0];
+        int M = front + values[1] - 1;
         
         for(int i = 2; i < values.size(); i++){
-            front[i-1] = max(front[i-2], values[i-1]+i-1);
-            M = max(front[i-1]+values[i]-i, M);
+            front = max(front, values[i-1]+i-1);
+            M = max(front+values[i]-i, M);
         }
         return M;
     }
