72e0c8141f60d32639cf788b387b1a1f504e6300
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-24 21:47:21 +0800

    Time: 217 ms (33.01%), Space: 14.9 MB (68.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -4,14 +4,12 @@ public:
         int m = matrix.size(), n = matrix[0].size();
         int x = 0, y = n-1;
         
-        while(true){
+        while(x != m && y != -1){
             if(target == matrix[x][y])
                 return true;
             if(target > matrix[x][y])
                 x++;
             else y--;
-            if(x == m || y == -1)
-                break;
         }
         return false;
     }
