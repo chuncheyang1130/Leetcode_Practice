1bced03dbc5b87e1fec6c662051e6030e2d7f856
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-28 22:41:21 +0800

    Time: 4 ms (89.73%), Space: 12.8 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -13,24 +13,20 @@ class Solution {
 public:
     TreeNode* traverse(TreeNode* root){
         TreeNode *rightLeaf = nullptr, *leftLeaf = nullptr;
-        //cout << root->val << endl;
         if(root->left == nullptr && root->right == nullptr)
             return root;
         
-        if(root->right != nullptr){
+        if(root->right != nullptr)
             rightLeaf = traverse(root->right);
-        }
+        
         if(root->left != nullptr){
             leftLeaf = traverse(root->left);
-            if(rightLeaf != nullptr){
+            if(rightLeaf != nullptr)
                 leftLeaf->right = root->right;
-                root->right = root->left;
-                root->left = nullptr;
-            }else{
-                root->right = root->left;
-                rightLeaf = leftLeaf;
-                root->left = nullptr;
-            }
+            else rightLeaf = leftLeaf;
+            
+            root->right = root->left;
+            root->left = nullptr;
         }
         return rightLeaf;
     }
