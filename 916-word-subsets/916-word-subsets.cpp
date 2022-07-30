2d4ca9f91d04f4931a7d518765bb566a80fc5ea5
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-07-31 01:43:36 +0800

    Time: 778 ms (17.98%), Space: 114 MB (29.21%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -3,26 +3,26 @@ public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
         vector<string> ans;
         
-        unordered_map<char, int> t1, t2, tmp;
+        unordered_map<char, int> table, tmp;
         
         for(int i = 0; i < words2.size(); i++){
             tmp.clear();
             for(int j = 0; j < words2[i].size(); j++)
                 tmp[words2[i][j]]++;
             for(auto it = tmp.begin(); it != tmp.end(); it++){
-                if(t2.find(it->first) == t2.end())
-                    t2[it->first] = it->second;
-                else t2[it->first] = max(it->second, t2[it->first]);
+                if(table.find(it->first) == table.end())
+                    table[it->first] = it->second;
+                else table[it->first] = max(it->second, table[it->first]);
             }
         }
         
         for(int i = 0; i < words1.size(); i++){
-            t1.clear();
+            tmp.clear();
             bool match = true;
             for(int j = 0; j < words1[i].size(); j++)
-                t1[words1[i][j]]++;
-            for(auto it = t2.begin(); it != t2.end(); it++){
-                if(t1.find(it->first) != t1.end() && t1[it->first] >= it->second)
+                tmp[words1[i][j]]++;
+            for(auto it = table.begin(); it != table.end(); it++){
+                if(tmp.find(it->first) != tmp.end() && tmp[it->first] >= it->second)
                     continue;
                 else{
                     match = false;
