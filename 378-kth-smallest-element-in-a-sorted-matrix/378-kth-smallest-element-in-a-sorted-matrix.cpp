53a6f866d8f842f4fcb1ebd95c897d4e1ebb0108
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-03 00:50:30 +0800

    Time: 48 ms (63.09%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,29 +1,49 @@
 class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         if(matrix.size() == 1)
+//             return matrix[0][0];
+        
+//         int n = matrix.size();
+//         vector<int> cur(n), cur_ind(n, 0);
+        
+//         for(int i = 0; i < n; i++)
+//             cur[i] = matrix[i][0];
+        
+        
+//         int ans, ind, m;
+//         for(int i = 1; i <= k; i++){
+//             ind = min_element(cur.begin(), cur.end())-cur.begin();
+//             m = cur[ind];
+            
+//             if(i == k)
+//                 ans = m;
+//             else{
+//                 cur_ind[ind]++;
+//                 if(cur_ind[ind] == n)
+//                     cur[ind] = INT_MAX;
+//                 else cur[ind] = matrix[ind][cur_ind[ind]];
+//             }
+//         }
+//         return ans;
         if(matrix.size() == 1)
-            return matrix[0][0];
+             return matrix[0][0];
         
         int n = matrix.size();
-        vector<int> cur(n), cur_ind(n, 0);
-        
-        for(int i = 0; i < n; i++)
-            cur[i] = matrix[i][0];
-        
-        
-        int ans, ind, m;
-        for(int i = 1; i <= k; i++){
-            ind = min_element(cur.begin(), cur.end())-cur.begin();
-            m = cur[ind];
+        int left = matrix[0][0], right = matrix[n-1][n-1];
+        int ans;
+        while(left <= right){
+            int mid = (left+right)/2;
+            cout << mid << " ";
+            int cnt = 0;
+            for(int i = 0; i < n; i++)
+                cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
             
-            if(i == k)
-                ans = m;
-            else{
-                cur_ind[ind]++;
-                if(cur_ind[ind] == n)
-                    cur[ind] = INT_MAX;
-                else cur[ind] = matrix[ind][cur_ind[ind]];
-            }
+            cout << cnt << endl;
+            if(cnt >= k){
+                right = mid - 1;
+                ans = mid;
+            }else left = mid + 1;
         }
         return ans;
     }
