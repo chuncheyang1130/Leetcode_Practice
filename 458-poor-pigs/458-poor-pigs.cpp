f55283cea496b58c3478f3a1b11042b6c2b5565d
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-06 21:52:35 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (22.55%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        return ceil(log(buckets)/log(minutesToTest / minutesToDie+1));
+        int t = minutesToTest / minutesToDie;
+        return ceil(log(buckets)/log(t+1));
     }
 };
