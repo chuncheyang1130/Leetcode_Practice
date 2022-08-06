e8de16c40a0056c4a211d909a57fd97f42443565
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-07 00:06:01 +0800

    Time: 170 ms (75.00%), Space: 54.8 MB (75.00%) - LeetHub

---
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long minimumReplacement(vector<int>& nums) {
+        if(nums.size() == 1)
+            return 0;
+        
+        long long total = 0;
+        for(int i = nums.size()-2; i >= 0; i--){
+            if(nums[i] > nums[i+1]){
+                int divide = ceil((double)nums[i] / nums[i+1]);
+                total += divide - 1;
+                nums[i] = nums[i] / divide;
+            }
+        }
+        return total;
+    }
+};
