6fd80b9cbe2d56997d4e4782e7496958d5608aa3
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-06 17:42:14 +0800

    Time: 21 ms (40.43%), Space: 9.6 MB (11.02%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        vector<vector<int>> row_sum(mat.size(), vector<int>(mat[0].size()+1, 0));
+        vector<vector<int>> sum(mat.size()+1, vector<int>(mat[0].size()+1, 0));
         vector<vector<int>> block_sum(mat.size(), vector<int>(mat[0].size(), 0));
-        for(int i = 0; i < mat.size(); i++){
-            for(int j = 1; j <= mat[i].size(); j++){
-                row_sum[i][j] = row_sum[i][j-1] + mat[i][j-1];
-            }
-        }
+        for(int i = 1; i <= mat.size(); i++)
+            for(int j = 1; j <= mat[i-1].size(); j++)
+                sum[i][j] = sum[i][j-1] + mat[i-1][j-1];
+        
+        for(int i = 2; i <= mat.size(); i++)
+            for(int j = 1; j <= mat[i-1].size(); j++)
+                sum[i][j] += sum[i-1][j];  
         
         int l, r, u, d;
         for(int i = 0; i < mat.size(); i++){
@@ -15,10 +17,9 @@ public:
                 l = (j-k>=0)?j-k:0;
                 r = (j+k<=mat[i].size()-1)?j+k+1:mat[i].size();
                 d = (i-k>=0)?i-k:0;
-                u = (i+k<=mat.size()-1)?i+k:mat.size()-1;
-                for(int m = d; m <= u; m++)
-                    block_sum[i][j] += row_sum[m][r] - row_sum[m][l];
+                u = (i+k<=mat.size()-1)?i+k+1:mat.size();
                 
+                block_sum[i][j] = sum[u][r] - sum[u][l] - sum[d][r] + sum[d][l];
             }
         }
         return block_sum;
