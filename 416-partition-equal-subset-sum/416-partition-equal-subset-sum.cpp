c35e12b3efbd884590c38e061bef248b48a5e0f2
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-09 17:24:52 +0800

    Time: 1323 ms (5.01%), Space: 37.9 MB (35.52%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -3,23 +3,23 @@ public:
     bool canPartition(vector<int>& nums) {
         int total = 0;
         
-        set<int> st;
+        unordered_map<int, int> table;
         vector<int> new_element;
         
         for(int i = 0; i < nums.size(); i++){
-            for(auto it = st.begin(); it != st.end(); it++)
-                new_element.push_back(*it+nums[i]);
+            for(auto it = table.begin(); it != table.end(); it++)
+                new_element.push_back(it->first+nums[i]);
             
             for(int i = 0; i < new_element.size(); i++)
-                st.insert(new_element[i]);
+                table[new_element[i]] = 1;
             
-            st.insert(nums[i]);
+            table[nums[i]] = 1;
             new_element.clear();
             total += nums[i];
         }
         
         if(total % 2)
             return false;
-        else return st.find(total/2) != st.end();
+        else return table.find(total/2) != table.end();
     }
 };
