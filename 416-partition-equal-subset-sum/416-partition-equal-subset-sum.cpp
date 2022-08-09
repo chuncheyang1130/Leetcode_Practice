995c697778456e443b361ff23e41efc1e7fe9bd8
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-09 17:58:10 +0800

    Time: 129 ms (86.10%), Space: 9.8 MB (86.03%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int total = 0;
-        vector<bool> table(40001, false);
         
         for(int i = 0; i < nums.size(); i++)
             total += nums[i];
         
         if(total % 2)
             return false;
+        
+        vector<bool> table(total+1, false);
         
         for(int i = 0; i < nums.size(); i++){
             for(int j = table.size()-1; j >= 1; j--)
