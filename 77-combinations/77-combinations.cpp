b2a054b9df015d0e9cd08bdc393675a1e4399683
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-10 22:27:13 +0800

    Time: 28 ms (81.89%), Space: 9.1 MB (82.14%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    void combine(vector<vector<int>>& ans, vector<bool>& used, vector<int>& cur, int pos, int len){
+    void combine(vector<vector<int>>& ans, vector<int>& cur, int pos, int len, int n){
         if(pos == len){
             ans.push_back(cur);
             return;
         }    
         
-        for(int i = cur[pos-1]+1; i < used.size(); i++){
-            if(!used[i]){
-                cur[pos] = i;
-                used[i] = true;
-                combine(ans, used, cur, pos+1, len);
-                used[i] = false; 
-            }
+        for(int i = cur[pos-1]+1; i <= n; i++){
+            cur[pos] = i;
+            combine(ans, cur, pos+1, len, n);
         }
     }
     
     vector<vector<int>> combine(int n, int k) {
-        vector<bool> used(n+1, false);
         vector<int> cur(k);
         vector<vector<int>> ans;
         
-        for(int i = 1; i < used.size(); i++){
+        for(int i = 1; i <= n; i++){
             cur[0] = i;
-            combine(ans, used, cur, 1, k);
+            combine(ans, cur, 1, k, n);
         }
         
         return ans;
