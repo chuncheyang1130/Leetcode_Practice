56e586e0454056258941496beab4b50b0a367100
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-15 12:42:50 +0800

    Time: 15 ms (62.96%), Space: 8.3 MB (21.24%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    map<char, int> table = {
+        {'M', 1000},
+        {'D', 500},
+        {'C', 100},
+        {'L', 50},
+        {'X', 10},
+        {'V', 5},
+        {'I', 1}
+    };
+    
+    int romanToInt(string s) {
+        int value = table[s[s.size()-1]];
+
+        for(int i = s.size()-2; i >= 0; i--){
+            if(table[s[i]] < table[s[i+1]])
+                value -= table[s[i]];
+            else    
+                value += table[s[i]];
+        }
+        
+        return value;
+    }
+};
