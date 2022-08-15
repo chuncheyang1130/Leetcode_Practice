598cc1f15c9efc297c2323432fc6aae9b72728f2
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-15 12:44:11 +0800

    Time: 22 ms (42.26%), Space: 7.7 MB (49.47%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    unordered_map<char, int> table = {
-        {'M', 1000},
-        {'D', 500},
-        {'C', 100},
-        {'L', 50},
-        {'X', 10},
-        {'V', 5},
-        {'I', 1}
-    };
-    
     int romanToInt(string s) {
+        unordered_map<char, int> table = {
+            {'M', 1000},
+            {'D', 500},
+            {'C', 100},
+            {'L', 50},
+            {'X', 10},
+            {'V', 5},
+            {'I', 1}
+        };
+    
         int value = table[s[s.size()-1]];
 
         for(int i = s.size()-2; i >= 0; i--){
