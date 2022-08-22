32e35a5b8c1eb7ca585aef2d98b7726ba07ba3cd
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-22 17:03:42 +0800

    Time: 2 ms (60.92%), Space: 6.1 MB (6.02%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n == 0)
             return false;
-        float power = log(n)/log(4);
+        double power = log(n)/log(4);
         if(power == floor(power))
             return true;
         else return false;
