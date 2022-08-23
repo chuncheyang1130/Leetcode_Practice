619317898688431d94ec5428c989202102ddaf8b
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-23 19:35:27 +0800

    Time: 47 ms (51.10%), Space: 27.7 MB (50.74%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
-        vector<int> ans;
-        int len_n = num.size(), len_k = (int)log10(k)+1, len = max(len_n, len_k);
+        
+        int len_n = num.size();
+        int len_k = (int)log10(k) + 1;
+        int len = max(len_n, len_k);
         int carry = 0;
         
+        vector<int> ans(len, 0);
+        
         for(int i = 1; i <= len; i++){
-            int n = carry;
+            ans[len-i] = carry;
             if(i <= len_n)
-                n += num[len_n-i];
+                ans[len-i] += num[len_n-i];
             if(k > 0){
-                n += (k % 10);
+                ans[len-i] += (k % 10);
                 k /= 10;
             }
             
-            carry = n / 10;
-            n %= 10;
-            ans.insert(ans.begin(), n);
+            carry = ans[len-i] / 10;
+            ans[len-i] %= 10;
         }
+        
         if(carry)
             ans.insert(ans.begin(), carry);
         return ans;
