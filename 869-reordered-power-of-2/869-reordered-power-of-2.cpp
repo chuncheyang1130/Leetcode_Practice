64502e52e2beddd9782c9264fe6b5ad9d5f71d0e
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-26 13:23:55 +0800

    Time: 3 ms (73.16%), Space: 6.2 MB (55.65%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    bool permute(string& value, vector<int>& digit, int pos){
-        if(pos == value.size()){
-            int n = stoi(value);
-            if(log2(n) == (int)log2(n))
-                return true;
-            else return false;
-        }
-        
-        for(int i = 0; i < digit.size(); i++){
-            if(pos == 0 && i == 0)
-                continue;
-            
-            if(digit[i]){
-                digit[i]--;
-                value[pos] = '0'+i;
-                bool powerOf2 = permute(value, digit, pos+1);
-                if(powerOf2)
-                    return true;
-                digit[i]++;
-            }
-        }
-        return false;
-    }
-    
     bool reorderedPowerOf2(int n) {
         vector<int> digit(10, 0);
-        string value;
-        
-        while(n > 0){
-            int d = n % 10;
+        int tmp_num = n;
+        while(tmp_num > 0){
+            int d = tmp_num % 10;
             digit[d]++;
-            value += " ";
-            n /= 10;
+            tmp_num /= 10;
         }
         
-        return permute(value, digit, 0);
+        long long base = 1;
+        while(floor(log10(base))+1 < floor(log10(n))+1)
+            base *= 2;
+        
+        while(floor(log10(base))+1 == floor(log10(n))+1){
+            vector<int> tmp = digit;
+            long long tmp_n = base;
+            bool match = true;
+            while(tmp_n > 0){
+                int d = tmp_n % 10;
+                if(tmp[d] == 0){
+                    match = false;
+                    break;
+                }else tmp[d]--;
+                
+                tmp_n /= 10;
+            }
+            if(match)
+                return true;
+            base *= 2;
+        }
+        return false;
+        
     }
 };
