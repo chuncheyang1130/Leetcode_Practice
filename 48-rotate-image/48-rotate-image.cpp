f932e18ff5f0b33203b234283cbdd89c8ccb7cf2
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-27 01:39:04 +0800

    Time: 8 ms (9.80%), Space: 7.3 MB (33.72%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -5,14 +5,12 @@ public:
         vector<vector<int>> tmp(n, vector<int>(n));
         
         for(int i = 0; i < n/2; i++){
-            for(int j = i; j < n-i; j++)
+            for(int j = i; j < n-i; j++){
                 tmp[j][n-i-1] = matrix[i][j]; 
-            for(int j = i; j < n-i; j++)
                 tmp[n-i-1][n-j-1] = matrix[j][n-i-1];
-            for(int j = i; j < n-i; j++)
                 tmp[n-j-1][i] = matrix[n-i-1][n-j-1];
-            for(int j = i; j < n-i; j++)
                 tmp[i][j] = matrix[n-j-1][i];
+            }
         }
         if(n % 2)
             tmp[n/2][n/2] = matrix[n/2][n/2];
