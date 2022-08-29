a62293137c0fa7fc567aa5730ea9c7fc474d8273
angusyang901130 <90640506+angusyang901130@users.noreply.github.com>
2022-08-29 16:48:07 +0800

    Time: 242 ms (5.01%), Space: 24.1 MB (6.13%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,40 +1,52 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>> visit(grid.size(), vector<bool>(grid[0].size(), false));
         int num = 0;
         
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(visit[i][j])
-                    continue;
-                else if(grid[i][j] == '0'){
-                    visit[i][j] = true;
-                    continue;
+        map<pair<int, int>, bool> land;
+        for(int i = 0; i < grid.size(); i++)
+            for(int j = 0; j < grid[0].size(); j++)
+                if(grid[i][j] == '1'){
+                    pair<int, int> p = pair<int,int>(i, j);
+                    land[p] = false;
                 }
+        
+        for(auto it = land.begin(); it != land.end(); it++){
+            if(it->second)
+                continue;
+            
+            stack<pair<int, int>> st;
+            st.push(it->first);
+            while(!st.empty()){
+                pair<int,int> top = st.top();
+                st.pop();
+                if(land[top])
+                    continue;
                 
-                stack<pair<int, int>> st;
-                st.push(pair<int,int>(i, j));
-                while(!st.empty()){
-                    pair<int, int> top = st.top();
-                    st.pop();
-                    if(visit[top.first][top.second])
-                        continue;
-                    
-                    //cout << top.first << " " << top.second << endl;
-                    visit[top.first][top.second] = true;
-                    if(top.first > 0 && !visit[top.first-1][top.second] && grid[top.first-1][top.second] == '1')
-                        st.push(pair<int, int>(top.first-1, top.second));
-                    if(top.first < grid.size()-1 && !visit[top.first+1][top.second] && grid[top.first+1][top.second] == '1')
-                        st.push(pair<int, int>(top.first+1, top.second));
-                    if(top.second > 0 && !visit[top.first][top.second-1] && grid[top.first][top.second-1] == '1')
-                        st.push(pair<int, int>(top.first, top.second-1));
-                    if(top.second < grid[0].size()-1 && !visit[top.first][top.second+1] && grid[top.first][top.second+1] == '1')
-                        st.push(pair<int, int>(top.first, top.second+1));
+                land[top] = true;
+                
+                if(top.first > 0){
+                    pair<int, int> p = pair<int, int>(top.first-1, top.second);
+                    if(land.find(p) != land.end() && !land[p])
+                        st.push(p);
                 }
-                //cout << endl;
-                num++;
+                if(top.first < grid.size()-1){
+                    pair<int, int> p = pair<int, int>(top.first+1, top.second);
+                    if(land.find(p) != land.end() && !land[p])
+                        st.push(p);
+                }
+                if(top.second > 0){
+                    pair<int, int> p = pair<int, int>(top.first, top.second-1);
+                    if(land.find(p) != land.end() && !land[p])
+                        st.push(p);
+                }
+                if(top.second < grid[0].size()-1){
+                    pair<int, int> p = pair<int, int>(top.first, top.second+1);
+                    if(land.find(p) != land.end() && !land[p])
+                        st.push(p);
+                }
             }
+            num++;
         }
         return num;
     }
