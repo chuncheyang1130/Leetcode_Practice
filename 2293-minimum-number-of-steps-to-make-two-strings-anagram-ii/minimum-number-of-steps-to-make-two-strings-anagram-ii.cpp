54154af01b6f09130faf09f0505fbe63e3c4c89d
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-01-20 02:07:08 +0800

    Time: 95 ms (94.54%) | Memory: 29.9 MB (7.62%) - LeetSync

--- 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        int s_cnt[26] = {0}, t_cnt[26] = {0};
+        int cnt[26] = {0};
     
         for (int i = 0; i < s.size(); i++)
-            s_cnt[s[i]-'a']++;
+            cnt[s[i]-'a']++;
 
         for (int i = 0; i < t.size(); i++)
-            t_cnt[t[i]-'a']++;
+            cnt[t[i]-'a']--;
         
-        int cnt = 0;
+        int step = 0;
         for (int i = 0; i < 26; i++)
-            cnt -= min(s_cnt[i], t_cnt[i]);
+            step += abs(cnt[i]);
 
-        cnt = 2 * cnt + (int)t.size() + (int)s.size();
-
-        return cnt;
+        return step;
 
     }
 };
