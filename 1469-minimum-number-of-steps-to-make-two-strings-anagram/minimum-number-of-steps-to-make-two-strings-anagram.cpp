8bb5f20405695a7e757bd99dc2803d9b3be79059
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-01-20 01:29:27 +0800

    Time: 43 ms (86.28%) | Memory: 17.8 MB (6.67%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -3,22 +3,19 @@ public:
     int minSteps(string s, string t) {
         int s_cnt[26] = {0}, t_cnt[26] = {0};
         int n = s.size();
+    
+        for (int i = 0; i < n; i++)
+            s_cnt[s[i]-'a']++;
 
-        int alph = 0;
-
-        for (int i = 0; i < n; i++){
-            alph = s[i] - 'a';
-            s_cnt[alph]++;
-
-            alph = t[i] - 'a';
-            t_cnt[alph]++;
-        }
+        for (int i = 0; i < n; i++)
+            t_cnt[t[i]-'a']++;
         
         int cnt = 0;
         for (int i = 0; i < 26; i++)
             cnt += abs(s_cnt[i]-t_cnt[i]);
 
-        return cnt/2;
+        cnt /= 2;
+        return cnt;
 
     }
 };
