63a2da383153c85e1fd1799bcf6f39cdde22d3da
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-01-20 01:33:49 +0800

    Time: 70 ms (34.34%) | Memory: 19.1 MB (6.67%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        int s_cnt[26] = {0}, t_cnt[26] = {0};
+        // int s_cnt[26] = {0}, t_cnt[26] = {0};
+        unordered_map<char, int> s_cnt, t_cnt;
         int n = s.size();
     
         for (int i = 0; i < n; i++)
-            s_cnt[s[i]-'a']++;
+            s_cnt[s[i]]++;
+            // s_cnt[s[i]-'a']++;
 
         for (int i = 0; i < n; i++)
-            t_cnt[t[i]-'a']++;
+            t_cnt[t[i]]++;
+            // t_cnt[t[i]-'a']++;
         
         int cnt = 0;
-        for (int i = 0; i < 26; i++)
-            cnt += abs(s_cnt[i]-t_cnt[i]);
+        for (char alph = 'a'; alph <= 'z'; alph++)
+            cnt += abs(s_cnt[alph]-t_cnt[alph]);
 
         cnt /= 2;
 
