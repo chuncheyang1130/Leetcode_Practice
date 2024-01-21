7b5cc74dbc719eb089785f78978b3bd997f40e37
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-01-21 13:02:09 +0800

    Time: 4 ms (25.45%) | Memory: 9 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-
-        vector<int> dp(nums.size()+1, 0);
-        dp[1] = nums[0];
         
-        for (int i = 1; i < nums.size(); i++){
-            dp[i+1] = max(dp[i], dp[i-1]+nums[i]);
+        if (nums.size() == 1)
+            return nums[0];
+
+        nums[1] = max(nums[1], nums[0]);
+
+        for (int i = 2; i < nums.size(); i++){
+            nums[i] = max(nums[i-1], nums[i-2]+nums[i]);
         }
 
-        return dp[nums.size()];
+        return nums.back();
     }
 };
