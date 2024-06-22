826740e4a96866bf4b9b0631fc72e694106df4c0
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-06-23 03:35:42 +0800

    Time: 75 ms (97.86%), Space: 70.2 MB (30.51%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -3,20 +3,28 @@ public:
     int numberOfSubarrays(vector<int>& nums, int k) {
 
         int count = 0;        
-        vector<int> odd;
-        odd.push_back(-1);
+        // vector<int> odd;
+        // odd.push_back(-1);
+
+        int odd[50002];
+        int idx = 0;
+        odd[idx++] = -1;
 
         for (int i = 0; i < nums.size(); i++){
+            // if (nums[i] % 2)
+            //     odd.push_back(i);
             if (nums[i] % 2)
-                odd.push_back(i);
+                odd[idx++] = i;
+            
         }
 
-        odd.push_back(nums.size());
+        // odd.push_back(nums.size());
+        odd[idx++] = nums.size();
 
         int l = 0;
         int r = k+1;
 
-        while (r < odd.size()){
+        while (r < idx){
             count += (odd[l+1] - odd[l]) * (odd[r] - odd[r-1]);
             r++;
             l++;
