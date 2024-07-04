b606898b41c7d9fcc7136eb610a84554bf5f8298
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-05 00:38:59 +0800

    Time: 4 ms (55.57%), Space: 14.7 MB (15.76%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+
+        if (nums.size() == 1)
+            return;
+
+        int inc_pos = -1;
+
+        int count[101] = {0};
+        int sub_count[101] = {0};
+
+        for (int i = 0; i < nums.size(); i++)
+            count[nums[i]]++;
+
+        for (int i = 0; i < nums.size()-1; i++){
+            if (nums[i] < nums[i+1]){
+                inc_pos = i;
+
+                for (int j = 0; j <= 100; j++){
+                    if (sub_count[j] > 0){
+                        count[j] -= sub_count[j];
+                        sub_count[j] = 0;
+                    }
+                }
+
+                sub_count[nums[i]]++;
+            }else {
+                sub_count[nums[i]]++;
+            }
+        }
+
+        int min_n = 0;
+
+        if (inc_pos == -1){
+            for (int i = 0; i < nums.size(); i++){
+                for (int n = min_n; n <= 100; n++){
+                    if (count[n] > 0){
+                        // cout << n << endl;
+                        nums[i] = n;
+                        count[n]--;
+                        min_n = n;
+                        break;
+                    }
+                }                
+            }
+            return;
+        }
+        
+        int found = false;
+        for (int n = 0; n <= 100; n++){
+            if (count[n] > 0 && found){
+                nums[inc_pos] = n;
+                count[n]--;
+                break;
+            }else if (count[n] > 0 && n == nums[inc_pos])
+                found = true;
+        }
+
+        for (int pos = inc_pos+1; pos < nums.size(); pos++){
+            for (int n = min_n; n <= 100; n++){
+                if (count[n] > 0){
+                    nums[pos] = n;
+                    count[n]--;
+                    min_n = n;
+                    break;
+                }
+            }
+        }
+    }
+};
