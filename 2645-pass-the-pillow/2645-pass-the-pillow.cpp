73a2d14224a27fe93addcf4b098da1a905fd2ff2
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-07 02:32:00 +0800

    Time: 2 ms (54.14%), Space: 7.1 MB (71.18%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int passThePillow(int n, int time) {
         
-        int round_time = (n-1) * 2;
-        int remain_time = time % round_time;
+        int remain_time = time % ((n-1) * 2);
 
         if (remain_time <= n-1)
             return remain_time + 1;
