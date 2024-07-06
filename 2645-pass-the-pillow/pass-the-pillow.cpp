3e150f3230941a320887200c376bd25eb4dcc1b9
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-07 02:31:26 +0800

    Time: 0 ms (100.00%) | Memory: 7.2 MB (30.58%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        
+        int round_time = (n-1) * 2;
+        int remain_time = time % round_time;
+
+        bool one_way = false;
+        if (remain_time <= n-1)
+            return remain_time + 1;
+        else return n - remain_time % (n-1); 
+
+    }
+};
