254040a11ae8ed7877dd608b08f0a151c35e5feb
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-07 02:24:57 +0800

    Time: 164 ms (65.95%), Space: 115.9 MB (58.3%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -20,30 +20,39 @@ public:
 
         vector<int> crit_pt;
 
+        int left_pt = -1;
+        int prev_pt = -1;
+
         int node_num = 1;
         
         while (right != nullptr){
-            if (left->next->val > left->val && left->next->val > right->val)
-                crit_pt.push_back(node_num);
-            else if (left->next->val < left->val && left->next->val < right->val)
-                crit_pt.push_back(node_num);
+            if (left->next->val > left->val && left->next->val > right->val){
+                if (left_pt == -1)
+                    left_pt = node_num;
+                
+                if (prev_pt != -1)
+                    ans[0] = min(ans[0], node_num-prev_pt);
+
+                prev_pt = node_num;
+            }
+            else if (left->next->val < left->val && left->next->val < right->val){
+                if (left_pt == -1)
+                    left_pt = node_num;
+
+                if (prev_pt != -1)
+                    ans[0] = min(ans[0], node_num-prev_pt);
+
+                prev_pt = node_num;
+            }
             
             left = left->next;
             right = right->next;
             node_num++;
         }
-
-        // for (int i = 0; i < crit_pt.size(); i++)
-        //     cout << crit_pt[i] << " ";
-
-        sort(crit_pt.begin(), crit_pt.end());
-
-        for (int i = 1; i < crit_pt.size(); i++)
-            ans[0] = min(ans[0], crit_pt[i] - crit_pt[i-1]);
         
         if (ans[0] == INT_MAX)
             ans[0] = -1;
-        else ans[1] = crit_pt.back() - crit_pt.front();
+        else ans[1] = prev_pt - left_pt;
 
         return ans;
     }
