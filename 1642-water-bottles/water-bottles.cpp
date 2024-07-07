1b59ae8bf940068e9e660920e9574d932126517d
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-08 02:00:46 +0800

    Time: 2 ms (53.84%) | Memory: 7.2 MB (30.43%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -3,12 +3,10 @@ public:
     int numWaterBottles(int numBottles, int numExchange) {
         
         int total = numBottles;
-        int new_exchange = 0;
 
         while (numBottles / numExchange){
-            new_exchange = numBottles / numExchange;
-            total += new_exchange; 
-            numBottles = numBottles % numExchange + new_exchange;
+            total += numBottles / numExchange; 
+            numBottles = numBottles % numExchange + numBottles / numExchange;
         }
 
         return total;
