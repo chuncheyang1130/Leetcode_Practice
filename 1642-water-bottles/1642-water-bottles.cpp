c99ce64adf6847e335d2de5126221741dfece685
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-08 01:58:49 +0800

    Time: 3 ms (13.78%), Space: 7.1 MB (72.3%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.cpp
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -4,12 +4,10 @@ public:
         
         int remain = numBottles;
         int total = numBottles;
-        int new_exchange;
 
         while (remain / numExchange){
-            new_exchange = remain / numExchange;
-            total += new_exchange; 
-            remain = remain % numExchange + new_exchange;
+            total += remain / numExchange; 
+            remain = remain % numExchange + remain / numExchange;
         }
 
         return total;
