f6cdb460a415be5ecb3c7c869a4e0f5b694adb69
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-10 00:35:21 +0800

    Time: 0 ms (100%), Space: 7.5 MB (49.72%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxBottlesDrunk(int numBottles, int numExchange) {
+        int total = 0;
+
+        while (numBottles / numExchange){
+            total += numExchange;
+            numBottles = numBottles - numExchange + 1;
+            numExchange++;
+        }
+
+        if (numBottles)
+            total += numBottles;
+
+        return total;
+    }
+};
