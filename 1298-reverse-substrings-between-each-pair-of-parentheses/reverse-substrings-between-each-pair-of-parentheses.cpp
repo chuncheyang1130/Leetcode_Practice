fdc038d99d59e38556546ab8de2c6796d2d5a810
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-12 03:49:13 +0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (49.65%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -3,22 +3,19 @@ public:
     string reverseParentheses(string s) {
         string str = "";
         stack<int> stk;
-        int space = 0;
 
         for (int i = 0; i < s.size(); i++){
             if (s[i] != '(' && s[i] != ')'){
                 str.push_back(s[i]);
             }else{
                 if (s[i] == '('){
-                    stk.push(i-space);
+                    stk.push(str.size());
                 }else{
                     reverse(str.begin()+stk.top(), str.end());
                     stk.pop();
                 }
-                space++;
             }
         }
-
 
         return str;
     }
