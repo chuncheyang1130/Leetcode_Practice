d5405acc4b05b0b77f1ede83ac4606fb1b8dccf9
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-12 03:46:58 +0800

    Time: 0 ms (100.00%) | Memory: 7.9 MB (48.42%) - LeetSync

--- 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     string reverseParentheses(string s) {
-        vector<int> order;
+        string str = "";
         stack<int> stk;
         int space = 0;
 
         for (int i = 0; i < s.size(); i++){
             if (s[i] != '(' && s[i] != ')'){
-                order.push_back(i);
+                str.push_back(s[i]);
             }else{
                 if (s[i] == '('){
                     stk.push(i-space);
                 }else{
-                    reverse(order.begin()+stk.top(), order.end());
+                    reverse(str.begin()+stk.top(), str.end());
                     stk.pop();
                 }
                 space++;
             }
         }
-
-        string str = "";
-        for (int i = 0; i < order.size(); i++)
-            str.push_back(s[order[i]]);
         
 
         return str;
