6bfb3ea2465f657056039e13fa3378782c5f19e1
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-07-24 22:13:00 +0800

    Time: 17 ms (59.39%), Space: 16.8 MB (31.9%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int findParent(vector<int>& parent, int src){
+        if (parent[src] != src)
+            parent[src] = findParent(parent, parent[src]);
+
+        return parent[src];
+    }
+
+    static bool cmp(vector<int>& v1, vector<int>& v2){
+        return v1[0] < v2[0];
+    }
+
+    int earliestAcq(vector<vector<int>>& logs, int n) {
+        int timestamp = 0;
+
+        vector<int> parent(n);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        sort(logs.begin(), logs.end(), cmp);
+
+        // for (int i = 0; i < logs.size(); i++){
+        //     cout << logs[i][0] << " ";
+        // }
+
+        // cout << endl;
+
+        int p1, p2;
+        for (int i = 0; i < logs.size(); i++){
+            p1 = findParent(parent, logs[i][1]);
+            p2 = findParent(parent, logs[i][2]);
+
+            // cout << "=====" << endl;
+            // cout << logs[i][1] << " " << p1 << endl;
+            // cout << logs[i][2] << " " << p2 << endl;
+            
+            if (p1 == p2)
+                continue;
+            else{
+                if (p1 > p2)
+                    parent[p1] = p2;
+                else if (p1 < p2)
+                    parent[p2] = p1;   
+
+                timestamp = logs[i][0];
+            }
+        }
+
+        for (int i = 1; i < n; i++){
+            if (findParent(parent, i) != 0)
+                return -1;
+        }
+
+        return timestamp;
+    }
+};
