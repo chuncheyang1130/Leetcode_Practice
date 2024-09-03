7eae7c80b0572504c608137c8b3d114332a1162d
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-09-04 02:00:49 +0800

    Time: 4 ms (39.15%), Space: 8.2 MB (68.72%) - LeetHub

---
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+
+        string convert;
+
+        for (int i = s.size()-1; i >= 0; i--) {
+            int n = s[i] - 'a' + 1;
+
+            while (n / 10) {
+                convert.insert(convert.begin(), '0'+n%10);
+                n /= 10;
+            }
+            
+            if (n != 0)
+                convert.insert(convert.begin(), '0'+n);
+        }
+        
+        int total = 0;
+        while (k--){
+            for (int i = convert.size()-1; i >= 0; i--)
+                total += convert[i] - '0';
+            
+            if (k == 0)
+                break;
+            
+            convert = "";
+            while (total / 10){
+                convert.insert(convert.begin(), '0'+total%10);
+                total /= 10;
+            }
+
+            if (total != 0)
+                convert.insert(convert.begin(), '0'+total);
+
+            total = 0;
+        }
+
+        return total;
+    }
+};
