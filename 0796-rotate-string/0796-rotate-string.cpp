fa759b33c969bc9e14f101c8ff62127366755814
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-11-04 01:04:47 +0800

    Time: 0 ms (100%), Space: 7.6 MB (55.81%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        
         for (int i = 0; i < goal.size(); i++){
             if (s == goal){
                 return true;
