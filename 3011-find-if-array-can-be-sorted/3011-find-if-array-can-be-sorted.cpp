bab719833124a4774b131427ca6648a50b2667d1
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-11-07 02:17:43 +0800

    Time: 0 ms (100%), Space: 31.1 MB (86.44%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     
     bool canSortArray(vector<int>& nums) {
-        vector<int> n_sb(nums.size(), 0);
+        if (nums.size() == 1)
+            return true;
 
-        int base = 1;
-        for (int i = 0; i <= 8; i++){
-            for (int j = 0; j < nums.size(); j++){
-                n_sb[j] += (nums[j] & base) >> i;
+        int n_sb = 0;
+        int cur_min = nums[0];
+        int cur_max = nums[0];
+        int prev_max = 0;
+
+        for (int i = 0; i <= 8; i++)
+            n_sb += (nums[0] & (1 << i)) >> i;  
+        
+        for (int i = 1; i < nums.size(); i++){
+            int tmp = 0;
+            for (int j = 0; j <= 8; j++)
+                tmp += (nums[i] & (1 << j)) >> j; 
+
+            if (tmp == n_sb){
+                cur_min = min(cur_min, nums[i]);
+                cur_max = max(cur_max, nums[i]);
+            }else {
+                if (cur_min < prev_max)
+                    return false;
+                n_sb = tmp;
+                prev_max = cur_max;
+                cur_min = nums[i];
+                cur_max = nums[i];
             }
-            base *= 2;
         }
+        
+        if (cur_min < prev_max)
+            return false;
 
-        for (int i = 0; i < nums.size()-1; i++){
-            for (int j = 0; j < nums.size()-1-i; j++){
-                if (nums[j] > nums[j+1] && n_sb[j] == n_sb[j+1])
-                    swap(nums[j], nums[j+1]);
-            }
-        }
-
-        for (int i = 0; i < nums.size()-1; i++){
-            if (nums[i] > nums[i+1])
-                return false;
-        }
         return true;
     }
 };
