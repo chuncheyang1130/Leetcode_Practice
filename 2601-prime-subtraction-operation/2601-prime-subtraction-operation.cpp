cd16a1fb3975dbf52549404e76ac71291aa15d59
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2024-11-12 02:49:52 +0800

    Time: 7 ms (41.97%), Space: 28.6 MB (55.27%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool primeSubOperation(vector<int>& nums) {
+        vector<int> prime_vec;
+
+        for (int n = 1000; n >= 2; n--){
+            int sq_root = sqrt(n);
+
+            bool isPrime = true;
+            for (int div = 2; div <= sq_root; div++){
+                if (n % div == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+                prime_vec.push_back(n);
+        }
+
+        prime_vec.push_back(0);
+        
+        bool inc = true;
+        for (int i = 0; i < nums.size(); i++){
+            auto begin_it = lower_bound(prime_vec.begin(), prime_vec.end(), nums[i], greater<int>());
+
+            int prev = 0;
+            if (i > 0)
+                prev = nums[i-1];
+
+            for (auto it = begin_it; it != prime_vec.end(); it++){
+                if (nums[i] - *it > prev){
+                    nums[i] -= *it;
+                    break;
+                }
+            }
+
+            // cout << nums[i] << endl;
+
+            if (nums[i] <= prev){
+                inc = false;
+                break;
+            }
+        }
+
+        return inc;
+    }
+};
