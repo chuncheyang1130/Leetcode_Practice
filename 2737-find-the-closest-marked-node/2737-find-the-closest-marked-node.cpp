b2ba040c0840f18c5077093f5964ff0f230eb4a8
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2025-03-25 03:54:06 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2737-find-the-closest-marked-node/2737-find-the-closest-marked-node.cpp
+++ 2737-find-the-closest-marked-node/2737-find-the-closest-marked-node.cpp
@@ -2,17 +2,40 @@ class Solution {
 public:
     int minimumDistance(int n, vector<vector<int>>& edges, int s, vector<int>& marked) {
         vector<int> dist(n, 1e9);
+        vector<vector<pair<int,int>>> edge_matrix(n, vector<pair<int,int>>());
         dist[s] = 0;
         
         int src, dst, wgt;
         int min_cost = 1e9;
-        while (n--){
-            for (int i = 0; i < edges.size(); i++){
-                src = edges[i][0];
-                dst = edges[i][1];
-                wgt = edges[i][2];
 
-                dist[dst] = min(dist[dst], dist[src] + wgt);
+        for (int i = 0; i < edges.size(); i++)
+            edge_matrix[edges[i][0]].push_back(make_pair(edges[i][2], edges[i][1]));
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<bool> visited(n, false);
+        pq.push(make_pair(dist[s], s));
+        
+        while (n--){
+            pair<int,int> p = pq.top();
+            pq.pop();
+
+            // cout << p.second << " " << p.first << endl;
+            if (visited[p.second])
+                continue;
+            
+            // cout << p.second << " " << p.first << endl;
+            visited[p.second] = true;
+            dist[p.second] = p.first;
+
+            for (int i = 0; i < edge_matrix[p.second].size(); i++){
+                int dst = edge_matrix[p.second][i].second;
+                int wgt = edge_matrix[p.second][i].first;
+
+                if (!visited[dst]){
+                    cout << p.first + wgt << " " << dst << endl;
+                    pq.push(make_pair(p.first+wgt, dst));
+                }
+
             }
         }
 
