bb86fe1f3239ccb89f130c63858a93fe70b93bce
chuncheyang1130 <90640506+chuncheyang1130@users.noreply.github.com>
2025-03-28 00:56:28 +0800

    Time: 42 ms (45.9%), Space: 98.6 MB (47.95%) - LeetHub

---
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        // Record number of each value
+        unordered_map<int, int> accum;
+        for (int i = 0; i < nums.size(); i++){
+            if (accum.find(nums[i]) == accum.end())
+                accum[nums[i]] = 1;
+            else accum[nums[i]]++;
+        }
+
+        // Find the number appears the most time
+        int target = nums[0];
+        for (auto it = accum.begin(); it != accum.end(); it++){
+            if (it->second > accum[target])
+                target = it->first;
+        }
+
+        if (accum[target] < (nums.size()+1)/2 + 1)
+            return -1;
+        
+        int cur_total = 0, idx = 0;
+        for (idx = 0; idx < nums.size(); idx++){
+            if (nums[idx] == target)
+                cur_total++;
+            if (cur_total > (idx+1)/2)
+                break;
+        }
+
+        return idx;
+    }
+};
